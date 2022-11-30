a0d631af006bff7357254a7f63320b1867a4bb50
dsLeks <alekhyahello@gmail.com>
2022-11-30 00:06:57 +0000

    making tests

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { select } from "d3";
   
+
 const Dashboard = () => {
+  const [data, setData] = useState([25,30,45,60,55]);
+  const svgRef = useRef();
+  
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg.selectAll("circle")
+       .data(data)
+       .join(
+          enter => enter
+                    .append("circle").attr("class", "new")
+                    .attr('cx', value => value * 5)
+                    .attr('cy', value => value * 5)
+                    .attr('r', value => value)
+                    .attr('stroke', "red")
+                    .attr('padding', '50px'),
+          update => update.attr("class", "updated"),
+          exit => exit.remove()
+       )
+  }, [data])
+  
   return (
-    <div>
-      <h1>Dashboard Page</h1>
-    </div>
+    <>
+    <svg ref={ svgRef } style={{border: '10px solid green'}}></svg>
+    <button onClick={() => setData(data.map(value => value + 5))}>Update data</button>
+    <button onClick={() => setData(data.filter(value => value < 35))}>Filter data</button>
+    </>
   );
 };
   
